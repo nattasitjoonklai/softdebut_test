e46f1f7748b2372c7adaeb0f5b9525524527d299
Nattasit <nattasitjoonklai@gmail.com>
2025-11-20 11:02:17 +0700

    update url

--- tests/login.js
+++ tests/login.js
@@ -1,7 +1,7 @@
 import { data_test } from "./Data_test";
 export async function login_homepage(page ) {
-  
-  await page.goto('') 
+  const   baseURL='https://opensource-demo.orangehrmlive.com/'
+  await page.goto(baseURL) 
   await page.waitForLoadState('networkidle'); //รอจนกว่า network load state เสร็จสิ้นแล้ว ใช้ในกรณี รันเคสมาก
   await page.getByRole('textbox', { name: 'username' }).fill(data_test.username); //ดึงค่าจากไฟล์ Data_test.js กรณีไม่มีไฟล์Json
   await page.getByRole('textbox', { name: 'password' }).fill(data_test.password);
